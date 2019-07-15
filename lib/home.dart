8ab337b0c3b61fe7be72b771eeb2783191e49519
amienhshaari <40850634+amienhshaari@users.noreply.github.com>
2019-07-15 12:11:11 +0800

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -6,22 +6,6 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-      ),
-      home: Home(),
-    );
-  }
-}
-
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
